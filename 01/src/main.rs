cf4c05a53fee8b667871909369332c6caebfb501
madetara <x@madetara.dev>
2021-12-02 09:59:38 +0300

    refactor: change answer naming convention

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -6,8 +6,8 @@ fn main() {
     let input = io::read_input();
 
     let part1 = solver::solve_part1(&input);
-    fs::write("part1.txt", part1.to_string()).unwrap();
-
     let part2 = solver::solve_part2(&input);
-    fs::write("part2.txt", part2.to_string()).unwrap();
+
+    fs::write("1.txt", part1.to_string()).unwrap();
+    fs::write("2.txt", part2.to_string()).unwrap();
 }
